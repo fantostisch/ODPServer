ab6fd5116927a522003647058e39524bbe01fe33
Nick Aquina <nickaquina@gmail.com>
2022-10-16 11:10:30 +0200

    Remove unused ToJSON from ODPClient and reorder

--- app/ODPClient.hs
+++ app/ODPClient.hs
@@ -8,29 +8,20 @@ module ODPClient
   )
 where
 
-import Data.Aeson (FromJSON, ToJSON (..), defaultOptions, genericToEncoding)
+import Data.Aeson (FromJSON)
 import Data.Text (Text)
 import GHC.Generics
 
 data ODPClient = Host Host | Follower Follower deriving (Generic, Show)
 
-instance FromJSON ODPClient
-
-instance ToJSON ODPClient where
-  toEncoding = genericToEncoding defaultOptions
-
 type HostID = Text
 
 newtype Host = HostC {id :: HostID} deriving (Generic, Show)
 
-instance FromJSON Host
-
-instance ToJSON Host where
-  toEncoding = genericToEncoding defaultOptions
-
 newtype Follower = FollowerC {hostToFollow :: HostID} deriving (Generic, Show)
 
-instance FromJSON Follower
+instance FromJSON ODPClient
 
-instance ToJSON Follower where
-  toEncoding = genericToEncoding defaultOptions
+instance FromJSON Host
+
+instance FromJSON Follower
